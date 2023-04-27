73d952bf9c0f7b2cabcd93ce3a25aee3b004e510
raul saavedra <raul.saavedra@gmail.com>
2023-04-27 17:43:38 +0200

    Updated fxp_xtimes, now also measuring relative execution times of all logarithms and all power functions

--- fxp_xtimes.c
+++ fxp_xtimes.c
@@ -17,7 +17,7 @@
 #include "fxp_aux.h"
 
 #define DASHES "=================================================\n"
-#define MAX_NUMS 1000
+#define MAX_NUMS 500
 #define MAX_OPS  100
 
 int main(void) {
@@ -26,11 +26,13 @@ int main(void) {
 
         long double tadd, tadd_l, tmul, tmul_l, tmul_d, tdiv, tdiv_l;
         long double tlg2, tlg2_l, tlg2_mul, tlg2_mul_l, tln, tln_l;
-        long double tpow2, tpow2_l, texp, texp_l;
+        long double tlg10, tlg10_l;
+        long double tpow2, tpow2_l, texp, texp_l, tpow10, tpow10_l;
         long double avgadd, avgadd_l, avgmul, avgmul_l, avgmul_d;
         long double avgdiv, avgdiv_l, avglg2, avglg2_l, avglg2_mul;
-        long double avglg2_mul_l, avgln, avgln_l;
+        long double avglg2_mul_l, avgln, avgln_l, avglg10, avglg10_l;
         long double avgpow2, avgpow2_l, avgexp, avgexp_l;
+        long double avgpow10, avgpow10_l;
         // Times for the system's native operations
         long double tadd_sys, tmul_sys, tdiv_sys;
         long double avgadd_sys, avgmul_sys, avgdiv_sys;
@@ -65,10 +67,14 @@ int main(void) {
         avglg2_mul_l = 0.0;
         avgln = 0.0;
         avgln_l = 0.0;
+        avglg10 = 0.0;
+        avglg10_l = 0.0;
         avgpow2 = 0.0;
         avgpow2_l = 0.0;
         avgexp = 0.0;
         avgexp_l = 0.0;
+        avgpow10 = 0.0;
+        avgpow10_l = 0.0;
         avgadd_sys = 0.0;
         avgmul_sys = 0.0;
         avgdiv_sys = 0.0;
@@ -88,10 +94,14 @@ int main(void) {
                 tlg2_mul_l = 0.0;
                 tln = 0.0;
                 tln_l = 0.0;
+                tlg10 = 0.0;
+                tlg10_l = 0.0;
                 tpow2 = 0.0;
                 tpow2_l = 0.0;
                 texp = 0.0;
                 texp_l = 0.0;
+                tpow10 = 0.0;
+                tpow10_l = 0.0;
                 lastp = -1;
                 tadd_sys = 0.0;
                 tmul_sys = 0.0;
@@ -331,13 +341,48 @@ int main(void) {
                         texp_l += dt;
                         avgexp_l += dt;
 
+                        // Calculation of pow10 of fxp's using
+                        // pow2() (only ints)
+                        t0 = clock();
+                        for (int i = 0; i < MAX_OPS; i++) {
+                                x = fxp_pow10(n1);
+                                x = fxp_pow10(n2);
+                                for (int j = 0; j < nvals; j++) {
+                                        y = val[j];
+                                        x = fxp_pow10(n1);
+                                        x = fxp_pow10(n2);
+                                        x = fxp_pow10(n3);
+                                }
+                        }
+                        t1= clock();
+                        dt = ((double) t1 - t0);
+                        tpow10 += dt;
+                        avgpow10 += dt;
+
+                        // Calculation of pow10_l of fxp's using
+                        // pow2_l() (-> longs)
+                        t0 = clock();
+                        for (int i = 0; i < MAX_OPS; i++) {
+                                x = fxp_pow10_l(n1);
+                                x = fxp_pow10_l(n2);
+                                for (int j = 0; j < nvals; j++) {
+                                        y = val[j];
+                                        x = fxp_pow10_l(n1);
+                                        x = fxp_pow10_l(n2);
+                                        x = fxp_pow10_l(n3);
+                                }
+                        }
+                        t1= clock();
+                        dt = ((double) t1 - t0);
+                        tpow10_l += dt;
+                        avgpow10_l += dt;
 
                         // To measure the lg execution use only + arguments
                         if (n1 < 0) n1 = -n1;
                         if (n2 < 0) n2 = -n2;
                         if (n3 < 0) n3 = -n3;
 
-                        // Calculation of lg2 of fxp's
+                        // Calculation of lg2 of fxp's using only ints
                         t0 = clock();
                         for (int i = 0; i < MAX_OPS; i++) {
                                 x = fxp_lg2(n1);
@@ -389,7 +434,7 @@ int main(void) {
                         tlg2_mul_l += dt;
                         avglg2_mul_l += dt;
 
-                        // Calculation of ln of fxp's
+                        // Calculation of ln of fxp's using ints
                         t0 = clock();
                         for (int i = 0; i < MAX_OPS; i++) {
                                 x = fxp_ln(n1);
@@ -406,7 +451,7 @@ int main(void) {
                         tln += dt;
                         avgln += dt;
 
-                        // Calculation of lg2 of fxp's using longs
+                        // Calculation of ln of fxp's using longs
                         t0 = clock();
                         for (int i = 0; i < MAX_OPS; i++) {
                                 x = fxp_ln_l(n1);
@@ -423,6 +468,39 @@ int main(void) {
                         tln_l += dt;
                         avgln_l += dt;
 
+                        // Calculation of lg10 of fxp's
+                        t0 = clock();
+                        for (int i = 0; i < MAX_OPS; i++) {
+                                x = fxp_lg10(n1);
+                                x = fxp_lg10(n2);
+                                for (int j = 0; j < nvals; j++) {
+                                        y = val[j];
+                                        x = fxp_lg10(n1);
+                                        x = fxp_lg10(n2);
+                                        x = fxp_lg10(n3);
+                                }
+                        }
+                        t1= clock();
+                        dt = ((double) t1 - t0);
+                        tlg10 += dt;
+                        avglg10 += dt;
+
+                        // Calculation of lg10 of fxp's using longs
+                        t0 = clock();
+                        for (int i = 0; i < MAX_OPS; i++) {
+                                x = fxp_lg10_l(n1);
+                                x = fxp_lg10_l(n2);
+                                for (int j = 0; j < nvals; j++) {
+                                        y = val[j];
+                                        x = fxp_lg10_l(n1);
+                                        x = fxp_lg10_l(n2);
+                                        x = fxp_lg10_l(n3);
+                                }
+                        }
+                        t1= clock();
+                        dt = ((double) t1 - t0);
+                        tlg10_l += dt;
+                        avglg10_l += dt;
                 }
 
                 // Results for this configuration of frag bits
@@ -431,9 +509,9 @@ int main(void) {
                 printf("mul_l    : %6.2Lf\n", tmul_l / tadd);
                 printf("div      : %6.2Lf\n", tdiv / tadd);
                 printf("div_l    : %6.2Lf\n", tdiv_l / tadd);
-                printf("lg2      : %6.2Lf  (BKM, only ints)\n", \
+                printf("lg2      : %6.2Lf  (BKM-L, only ints)\n", \
                             tlg2 / tadd);
-                printf("lg2_l    : %6.2Lf  (about %5.2Lfx lg2, using BKM and longs)\n", \
+                printf("lg2_l    : %6.2Lf  (about %5.2Lfx lg2, using BKM-L and longs)\n", \
                             tlg2_l / tadd, tlg2_l / tlg2);
                 printf("lg2_mul_l: %6.2Lf  (about %5.2Lfx lg2, using mult. and longs)\n", \
                             tlg2_mul_l / tadd, tlg2_mul_l / tlg2);
@@ -441,14 +519,22 @@ int main(void) {
                             tln / tadd);
                 printf("ln_l     : %6.2Lf  (about %5.2Lfx lg2, using lg2_l)\n", \
                             tln_l / tadd, tln_l / tlg2);
-                printf("pow2     : %6.2Lf  (BKM, only ints)\n", \
+                printf("lg10     : %6.2Lf  (using lg2)\n", \
+                            tlg10 / tadd);
+                printf("lg10_l   : %6.2Lf  (about %5.2Lfx lg2, using lg2_l)\n", \
+                            tlg10_l / tadd, tlg10_l / tlg2);
+                printf("pow2     : %6.2Lf  (BKM-E, only ints)\n", \
                             tpow2 / tadd);
-                printf("pow2_l   : %6.2Lf  (about %5.2Lfx pow2, using BKM and longs)\n", \
+                printf("pow2_l   : %6.2Lf  (about %5.2Lfx pow2, using BKM-E and longs)\n", \
                             tpow2_l / tadd, tpow2_l / tpow2);
                 printf("exp      : %6.2Lf  (about %5.2Lfx pow2, using pow2)\n", \
                             texp / tadd, texp / tpow2);
                 printf("exp_l    : %6.2Lf  (about %5.2Lfx pow2, using pow2_l)\n", \
                             texp_l / tadd, texp_l / tpow2);
+                printf("pow10    : %6.2Lf  (about %5.2Lfx pow2, using pow2)\n", \
+                            tpow10 / tadd, tpow10 / tpow2);
+                printf("pow10_l  : %6.2Lf  (about %5.2Lfx pow2, using pow2_l)\n", \
+                            tpow10_l / tadd, tpow2_l / tpow2);
         }
 
         // Overall results
@@ -468,11 +554,15 @@ int main(void) {
         avgdiv_l /= nconfigs;
         avglg2 /= nconfigs;
         avglg2_l /= nconfigs;
+        avglg10 /= nconfigs;
+        avglg10_l /= nconfigs;
         avgln /= nconfigs;
         avgln_l /= nconfigs;
         avglg2_mul_l /= nconfigs;
         avgpow2 /= nconfigs;
         avgpow2_l /= nconfigs;
+        avgpow10 /= nconfigs;
+        avgpow10_l /= nconfigs;
         avgexp /= nconfigs;
         avgexp_l /= nconfigs;
         printf("add      : %6.2Lf  (%6.2Lfx system's native addition of ints)\n", \
@@ -496,6 +586,10 @@ int main(void) {
                     avgln / avgadd, avgln / avglg2);
         printf("ln_l     : %6.2Lf  (about %5.2Lfx lg2, using lg2_l)\n", \
                     avgln_l / avgadd, avgln_l / avglg2);
+        printf("lg10     : %6.2Lf  (about %5.2Lfx lg2, using lg2)\n", \
+                    avglg10 / avgadd, avglg10 / avglg2);
+        printf("lg10_l   : %6.2Lf  (about %5.2Lfx lg2, using lg2_l)\n", \
+                    avglg10_l / avgadd, avglg10_l / avglg2);
         printf("pow2     : %6.2Lf  (BKM, only ints)\n", \
                     avgpow2 / avgadd);
         printf("pow2_l   : %6.2Lf  (about %5.2Lfx pow2, using BKM and longs)\n", \
@@ -507,6 +601,12 @@ int main(void) {
         printf("exp_l    : %6.2Lf  (about %5.2Lfx pow2, using pow2_l)\n", \
                     avgexp_l / avgadd, \
                     avgexp_l / avgpow2);
+        printf("pow10    : %6.2Lf  (about %5.2Lfx pow2, using pow2)\n", \
+                    avgpow10 / avgadd, \
+                    avgpow10 / avgpow2);
+        printf("pow10_l  : %6.2Lf  (about %5.2Lfx pow2, using pow2_l)\n", \
+                    avgpow10_l / avgadd, \
+                    avgpow10_l / avgpow2);
 
         printf("%s", DASHES);
         printf("(Keep in mind: compiler optimization options used/not used can ");
--- x
+++ x
@@ -4,8 +4,8 @@ clear
 if [ -f "x.out" ]; then
     rm x.out
 fi
-#gcc -O2 fxp.h fxp.c fxp_aux.h fxp_aux.c print_as_bits.h print_as_bits.c fxp_conv.h fxp_conv.c fxp_l.h fxp_l.c fxp_xtimes.c -lm -o x.out
-gcc fxp.h fxp.c fxp_aux.h fxp_aux.c print_as_bits.h print_as_bits.c fxp_conv.h fxp_conv.c fxp_l.h fxp_l.c fxp_xtimes.c -lm -o x.out
+#gcc -O2 fxp.h fxp.c ulongy.h ulongy.c fxp_aux.h fxp_aux.c print_as_bits.h print_as_bits.c fxp_conv.h fxp_conv.c fxp_l.h fxp_l.c fxp_xtimes.c -lm -o x.out
+gcc fxp.h fxp.c ulongy.h ulongy.c fxp_aux.h fxp_aux.c print_as_bits.h print_as_bits.c fxp_conv.h fxp_conv.c fxp_l.h fxp_l.c fxp_xtimes.c -lm -o x.out
 if [ -f "x.out" ]; then
     ./x.out
 fi
